4ef70ee37cd6e170baa21f7dd59fb8da8fa986a2
Dhruv Suri <dhruvsuri789@gmail.com>
2025-03-16 09:55:05 +0530

    Hidden Status Bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,17 +2,21 @@ import { Stack } from "expo-router";
 import "./globals.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StatusBar } from "react-native";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack>
-        {/* Hides routes group (tabs) header */}
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
-      </Stack>
+      <>
+        <StatusBar hidden={true} />
+        <Stack>
+          {/* Hides routes group (tabs) header */}
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
+        </Stack>
+      </>
     </QueryClientProvider>
   );
 }
